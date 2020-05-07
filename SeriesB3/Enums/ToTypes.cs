6b657a430d9984de221dff020e9b09b0a9468abb
André Oliveira <inforoliver@gmail.com>
2020-05-07 20:46:09 -0300

    Add styles

---
+++ SeriesB3/Enums/ToTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesB3
+{
+    public enum ToTypes
+    {
+        ToCsv,
+        ToSql
+    }
+}
---
+++ SeriesB3/Provider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesB3
+{
+    public class Provider : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ToTypes _toTypes;
+
+        public ToTypes ToType
+        {
+            get { return _toTypes; }
+            set
+            {
+                if (_toTypes != value)
+                {
+                    _toTypes = value;
+                    this.OnPropertyChanged("ToType");
+                }
+            }
+        }
+
+        public Provider()
+        {
+        }
+    }
+}
