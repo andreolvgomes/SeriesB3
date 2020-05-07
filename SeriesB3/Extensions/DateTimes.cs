2bc4adfe73e36e4afc039f1fb1b131e8e9445474
André Oliveira <inforoliver@gmail.com>
2020-05-06 23:49:08 -0300

    Added more one extension

---
+++ SeriesB3/Extensions/DateTimes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class DateTimes
+    {
+        //public static string ToDate(this DateTime datetime)
+        //{
+        //    return datetime.ToString();
+        //}
+    }
+}
